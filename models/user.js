77f5082ac784399273dc621c9188e22fe6d1f307
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-07-13 12:57:58 -0700

    need help

--- models/user.js
+++ models/user.js
@@ -2,26 +2,27 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, select: false },
+  username: { type: String, required: true },
+  password: { type: String, select: false },
 }, { timestamps: true });
 
 userSchema.pre('save', function (next) {
-    const user = this;
-    if (!user.isModified('password')) {
-        return next();
-    }
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(user.password, salt, (_, hash) => {
-            user.password = hash;
-            next();
-        });
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (_, hash) => {
+      user.password = hash;
+      next();
     });
+  });
 });
-    userSchema.methods.comparePassword = function (password, done) {
-        bcrypt.compare(password, this.password, (err, isMatch) => {
-            done(err, isMatch);
-        });
-    };
 
-    module.exports = model('User', userSchema);
+userSchema.methods.comparePassword = function (password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
+
+module.exports = model('User', userSchema);
